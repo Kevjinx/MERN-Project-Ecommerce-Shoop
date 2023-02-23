6c85b4dbd5355df28df45f4c05cab150eb516ab8
kevjinx <kevin415mei@gmail.com>
2023-02-23 07:47:19 -0800

    update header links

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
         <Container>
-          <Navbar.Brand href="#home">Shoop</Navbar.Brand>
+          {/* wrapping to make element behave like a Link from react-router, avoiding screen reloading */}
+          <LinkContainer to="/">
+            <Navbar.Brand href="/">Shoop</Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#cart">
-                <i className="fas fa-shopping-cart"></i>
-                Cart
-              </Nav.Link>
-              <Nav.Link href="#signin">
-                <i className="fas fa-sign-in"></i>
-                Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/signin">
+                <Nav.Link>
+                  <i className="fas fa-sign-in"></i>
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>

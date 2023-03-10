0e13306e1980a975c2695481d085e2948db4c151
kevjinx <kevin415mei@gmail.com>
2023-03-10 11:48:52 -0800

    add order routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,13 +2,19 @@ import express from 'express';
 import {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
   getMyOrders,
+  getAllOrders,
 } from '../controller/orderController.js';
 
 const orderRoutes = express.Router();
 
 orderRoutes.route('/').post(addOrderItems);
+orderRoutes.route('/').get(getAllOrders);
 orderRoutes.route('/:id').get(getOrderById);
 orderRoutes.route('/user').get(getMyOrders);
+orderRoutes.route('/:id/pay').put(updateOrderToPaid);
+orderRoutes.route('/:id/deliver').put(updateOrderToDelivered);
 
 export default orderRoutes;
